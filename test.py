3c0458385f9fce60e4cda58d3dcfb4653675f527
Gustavo C. Lima <87615776+limagustav0@users.noreply.github.com>
2025-07-16 12:16:22 -0300

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from crawl4ai import AsyncWebCrawler
 
 async def main():
     async with AsyncWebCrawler() as crawler:
-        result = await crawler.arun("https://www.epocacosmeticos.com.br/pesquisa?q=4064666318356")
+        result = await crawler.arun("https://www.epocacosmeticos.com.br/shampoo-invigo-color-brilliance-1l---wella-professionals-75980/p")
         print(result.markdown[:300])  # Print first 300 chars
 
 if __name__ == "__main__":
